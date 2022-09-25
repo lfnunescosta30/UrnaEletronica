dc2ba9a29196fd324045c140f060ecf38b25bad7
lfnunescosta30 <lf.nunes.costa@gmail.com>
2022-09-25 17:35:41 -0300

    Criando a interface do usuario.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_num_um = new System.Windows.Forms.Button();
+            this.btn_num_seis = new System.Windows.Forms.Button();
+            this.btn_num_cinco = new System.Windows.Forms.Button();
+            this.btn_num_quatro = new System.Windows.Forms.Button();
+            this.btn_num_tres = new System.Windows.Forms.Button();
+            this.btn_num_dois = new System.Windows.Forms.Button();
+            this.btn_num_zero = new System.Windows.Forms.Button();
+            this.btn_num_nove = new System.Windows.Forms.Button();
+            this.btn_num_sete = new System.Windows.Forms.Button();
+            this.btn_num_oito = new System.Windows.Forms.Button();
+            this.btn_branco = new System.Windows.Forms.Button();
+            this.btn_corrige = new System.Windows.Forms.Button();
+            this.btn_confirma = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_num_um
+            // 
+            this.btn_num_um.Location = new System.Drawing.Point(567, 110);
+            this.btn_num_um.Name = "btn_num_um";
+            this.btn_num_um.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_um.TabIndex = 0;
+            this.btn_num_um.Text = "1";
+            this.btn_num_um.UseVisualStyleBackColor = true;
+            this.btn_num_um.Click += new System.EventHandler(this.btn_num_um_Click);
+            // 
+            // btn_num_seis
+            // 
+            this.btn_num_seis.Location = new System.Drawing.Point(729, 176);
+            this.btn_num_seis.Name = "btn_num_seis";
+            this.btn_num_seis.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_seis.TabIndex = 1;
+            this.btn_num_seis.Text = "6";
+            this.btn_num_seis.UseVisualStyleBackColor = true;
+            // 
+            // btn_num_cinco
+            // 
+            this.btn_num_cinco.Location = new System.Drawing.Point(647, 176);
+            this.btn_num_cinco.Name = "btn_num_cinco";
+            this.btn_num_cinco.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_cinco.TabIndex = 2;
+            this.btn_num_cinco.Text = "5";
+            this.btn_num_cinco.UseVisualStyleBackColor = true;
+            // 
+            // btn_num_quatro
+            // 
+            this.btn_num_quatro.Location = new System.Drawing.Point(567, 176);
+            this.btn_num_quatro.Name = "btn_num_quatro";
+            this.btn_num_quatro.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_quatro.TabIndex = 3;
+            this.btn_num_quatro.Text = "4";
+            this.btn_num_quatro.UseVisualStyleBackColor = true;
+            // 
+            // btn_num_tres
+            // 
+            this.btn_num_tres.Location = new System.Drawing.Point(729, 110);
+            this.btn_num_tres.Name = "btn_num_tres";
+            this.btn_num_tres.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_tres.TabIndex = 4;
+            this.btn_num_tres.Text = "3";
+            this.btn_num_tres.UseVisualStyleBackColor = true;
+            // 
+            // btn_num_dois
+            // 
+            this.btn_num_dois.Location = new System.Drawing.Point(647, 110);
+            this.btn_num_dois.Name = "btn_num_dois";
+            this.btn_num_dois.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_dois.TabIndex = 5;
+            this.btn_num_dois.Text = "2";
+            this.btn_num_dois.UseVisualStyleBackColor = true;
+            this.btn_num_dois.Click += new System.EventHandler(this.btn_num_dois_Click);
+            // 
+            // btn_num_zero
+            // 
+            this.btn_num_zero.Location = new System.Drawing.Point(647, 307);
+            this.btn_num_zero.Name = "btn_num_zero";
+            this.btn_num_zero.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_zero.TabIndex = 6;
+            this.btn_num_zero.Text = "0";
+            this.btn_num_zero.UseVisualStyleBackColor = true;
+            // 
+            // btn_num_nove
+            // 
+            this.btn_num_nove.Location = new System.Drawing.Point(729, 243);
+            this.btn_num_nove.Name = "btn_num_nove";
+            this.btn_num_nove.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_nove.TabIndex = 7;
+            this.btn_num_nove.Text = "9";
+            this.btn_num_nove.UseVisualStyleBackColor = true;
+            // 
+            // btn_num_sete
+            // 
+            this.btn_num_sete.Location = new System.Drawing.Point(567, 243);
+            this.btn_num_sete.Name = "btn_num_sete";
+            this.btn_num_sete.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_sete.TabIndex = 8;
+            this.btn_num_sete.Text = "7";
+            this.btn_num_sete.UseVisualStyleBackColor = true;
+            // 
+            // btn_num_oito
+            // 
+            this.btn_num_oito.Location = new System.Drawing.Point(647, 243);
+            this.btn_num_oito.Name = "btn_num_oito";
+            this.btn_num_oito.Size = new System.Drawing.Size(45, 43);
+            this.btn_num_oito.TabIndex = 9;
+            this.btn_num_oito.Text = "8";
+            this.btn_num_oito.UseVisualStyleBackColor = true;
+            // 
+            // btn_branco
+            // 
+            this.btn_branco.Location = new System.Drawing.Point(522, 375);
+            this.btn_branco.Name = "btn_branco";
+            this.btn_branco.Size = new System.Drawing.Size(76, 43);
+            this.btn_branco.TabIndex = 10;
+            this.btn_branco.Text = "BRANCO";
+            this.btn_branco.UseVisualStyleBackColor = true;
+            // 
+            // btn_corrige
+            // 
+            this.btn_corrige.Location = new System.Drawing.Point(632, 375);
+            this.btn_corrige.Name = "btn_corrige";
+            this.btn_corrige.Size = new System.Drawing.Size(76, 43);
+            this.btn_corrige.TabIndex = 11;
+            this.btn_corrige.Text = "CORRIGE";
+            this.btn_corrige.UseVisualStyleBackColor = true;
+            // 
+            // btn_confirma
+            // 
+            this.btn_confirma.Location = new System.Drawing.Point(740, 375);
+            this.btn_confirma.Name = "btn_confirma";
+            this.btn_confirma.Size = new System.Drawing.Size(76, 43);
+            this.btn_confirma.TabIndex = 12;
+            this.btn_confirma.Text = "CONFIRMA";
+            this.btn_confirma.UseVisualStyleBackColor = true;
+            this.btn_confirma.Click += new System.EventHandler(this.btn_confirma_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(72, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 308);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(890, 583);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_confirma);
+            this.Controls.Add(this.btn_corrige);
+            this.Controls.Add(this.btn_branco);
+            this.Controls.Add(this.btn_num_oito);
+            this.Controls.Add(this.btn_num_sete);
+            this.Controls.Add(this.btn_num_nove);
+            this.Controls.Add(this.btn_num_zero);
+            this.Controls.Add(this.btn_num_dois);
+            this.Controls.Add(this.btn_num_tres);
+            this.Controls.Add(this.btn_num_quatro);
+            this.Controls.Add(this.btn_num_cinco);
+            this.Controls.Add(this.btn_num_seis);
+            this.Controls.Add(this.btn_num_um);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btn_num_um;
+        private Button btn_num_seis;
+        private Button btn_num_cinco;
+        private Button btn_num_quatro;
+        private Button btn_num_tres;
+        private Button btn_num_dois;
+        private Button btn_num_zero;
+        private Button btn_num_nove;
+        private Button btn_num_sete;
+        private Button btn_num_oito;
+        private Button btn_branco;
+        private Button btn_corrige;
+        private Button btn_confirma;
+        private PictureBox pictureBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,40 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace UrnaEletronica
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+       // string joao = "12";
+        //string pedro = "13";
+        //string paulo = "14";
+        string x = "";
+        string y = "";
+        string z = "";
+        private void btn_num_um_Click(object sender, EventArgs e)
+        {
+             x = "1";
+        }
+
+        private void btn_num_dois_Click(object sender, EventArgs e)
+        {
+            y = "2";
+            
+        }
+
+        private void btn_confirma_Click(object sender, EventArgs e)
+        {
+            z = x + y;
+            
+            if (z == "12")
+            {
+                MessageBox.Show("Você votou no joao");
+            }
         }
     }
 }
